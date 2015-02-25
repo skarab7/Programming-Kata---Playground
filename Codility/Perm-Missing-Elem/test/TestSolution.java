81d3e8be828e26a6ccc2774cc97264fa240d9de5
wb <wpjtek@yahoo.com>
2015-02-25 14:46:48 +0100

    Template files for Codility

---
+++ Codility/Perm-Missing-Elem/test/TestSolution.java
@@ -0,0 +1,22 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class TestSolution {
+
+    @Test
+    public void testSmallExample() {
+        Assert.assertTrue(false);   
+    }
+    
+    @Test
+    public void testBigInput() {
+        Assert.assertTrue(false);  
+    }
+    
+    @Test
+    public void testCornerCase1() {
+        Assert.assertTrue(false);  
+    }
+
+}
