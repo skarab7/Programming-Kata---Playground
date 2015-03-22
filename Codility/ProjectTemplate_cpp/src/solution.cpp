4f27f2e9e413402f40374f14dfe976b0625cdd09
wb <wpjtek@yahoo.com>
2015-03-22 20:04:49 +0100

    Good enought template for code challenges in C++

---
+++ Codility/ProjectTemplate_cpp/src/solution.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+class Solution {
+    public:
+        int getClusters(int i) {
+            return i + 1;
+        }
+};
+
+
+/* int main() {
+   Solution* s = new Solution();
+    std::cout<<"w:"<<s->getClusters(1);
+} */
