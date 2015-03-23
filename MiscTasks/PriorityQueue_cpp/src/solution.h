93b04c7b5cb3260396ee5e31d4051d8b02c6e0bb
wb <wpjtek@yahoo.com>
2015-03-23 23:46:16 +0100

    Add answers to the questions

--- MiscTasks/PriorityQueue_cpp/src/solution.h
+++ MiscTasks/PriorityQueue_cpp/src/solution.h
@@ -3,11 +3,24 @@
 // Given the above class declaration:
 // a) Implement the methods of PriorityQueue such that insert() is an O(1)
 //   operation and next() is O(n). (This is Big-O notation.)
+//
+//   Build a priorityQueue around a linked array (std::vector):
+//    
+//    - O(n) search for the max
+//    - O(1) having a pointer to the last element, we can simply add it 
+//
 //b) Implement the methods of PriorityQueue such that next() is an O(1)
 //   operation and insert() is O(n).
+//
+//   Build a priorityQueue around a Heap:
+//
+//   - O(1) next(), because we get the max elem on the top
+//   - O(n) is the worst case complexity of adding element to a heap 
+//
 // c) Suggest any changes to the class declaration that you think would
 //   be improvements.
-
+// 
+//   - a proper iterator, so we know whether there are elements left
 
 #ifndef PRIORITY_QUEUE_HXX
 #define PRIORITY_QUEUE_HXX
