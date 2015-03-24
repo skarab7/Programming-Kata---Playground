a41d67ef7682cc6beabe5c37b4158291c82edd7b
wb <wpjtek@yahoo.com>
2015-03-24 01:19:37 +0100

    Tab -> 4 spaces

--- MiscTasks/PriorityQueue_cpp/test/solution_unittest.cpp
+++ MiscTasks/PriorityQueue_cpp/test/solution_unittest.cpp
@@ -3,39 +3,39 @@
 #include <limits.h>
 
 TEST(Solution, OneString) {
-	PriorityQueue pq = PriorityQueue();
-	std::string x = "xxx";
-	pq.insert(20, x);
+    PriorityQueue pq = PriorityQueue();
+    std::string x = "xxx";
+    pq.insert(20, x);
     EXPECT_EQ(x, pq.next());
 }
 
 TEST(Solution, TwoStrings) {
-	PriorityQueue pq = PriorityQueue();
-	std::string x = "xxx";
-	std::string w = "www";
+    PriorityQueue pq = PriorityQueue();
+    std::string x = "xxx";
+    std::string w = "www";
 	
-	pq.insert(10, w);
-	pq.insert(20, x);
+    pq.insert(10, w);
+    pq.insert(20, x);
     EXPECT_EQ(x, pq.next());
-	EXPECT_EQ(w, pq.next());
+    EXPECT_EQ(w, pq.next());
     //How to handle when there are no elements? EXPECT_TRUE(NULL == pq.next);
 }
 
 TEST(Solution, TwoSamePrioString) {
-	PriorityQueue pq = PriorityQueue();
-	std::string x = "xxx";
-	std::string w = "www";
+    PriorityQueue pq = PriorityQueue();
+    std::string x = "xxx";
+    std::string w = "www";
     std::string y = "yyy";
 
-	pq.insert(20, x);
-	pq.insert(20, w);
+    pq.insert(20, x);
+    pq.insert(20, w);
     pq.insert(10, y);	
 	
     std::string call1 = pq.next();
     EXPECT_TRUE(call1 == w || call1 == x );
-	std::string call2 = pq.next();
+    std::string call2 = pq.next();
     EXPECT_TRUE(call2 == w || call2 == x );
-	//How to handle when there are no elements? EXPECT_TRUE(NULL == pq.next());
+    //How to handle when there are no elements? EXPECT_TRUE(NULL == pq.next());
     EXPECT_EQ(y, pq.next());
 }
 
@@ -54,7 +54,7 @@ TEST(Solution, GivenArrayWhenAllRemoveAndNextOnEmptyThenException) {
     pq.insert(30,"xw");
     pq.next();
     pq.next();
-     
+ 
     EXPECT_ANY_THROW({
         pq.next();
     });    
