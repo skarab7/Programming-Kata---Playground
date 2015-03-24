407af9d5d67a4604835b4a85a0f27681daa9779b
wb <wpjtek@yahoo.com>
2015-03-24 01:40:36 +0100

    Linted solution.cpp, check the naming conventions later

--- MiscTasks/PriorityQueue_cpp/src/solution.cpp
+++ MiscTasks/PriorityQueue_cpp/src/solution.cpp
@@ -5,8 +5,8 @@ PriorityQueue::PriorityQueue() {
 
 
 void PriorityQueue::insert(unsigned int priority, const std::string &str) {
-	PriorityQueueNode* pn = new PriorityQueueNode();
-	pn->data = str;
+    PriorityQueueNode* pn = new PriorityQueueNode();
+    pn->data = str;
     pn->priority = priority;
     m_Queue.push_back(pn);
     bubbleUp();
@@ -16,94 +16,99 @@ void PriorityQueue::insert(unsigned int priority, const std::string &str) {
 void PriorityQueue::bubbleUp() {
      int parent_offset;
      int child_offset = m_Queue.size() - 1;
-    
+
      std::vector<PriorityQueueNode*>::iterator it_parent;
      std::vector<PriorityQueueNode*>::iterator it_child;
-     
 
-     while(child_offset > 0) {
-         parent_offset = (int) floor( (double) child_offset/2);
-         it_parent = m_Queue.begin() + parent_offset;
-         it_child = m_Queue.begin() + child_offset;
-         
-         if( (*it_parent)->priority < (*it_child)->priority) {
+
+     while (child_offset > 0) {
+        parent_offset = static_cast<int>(
+            floor(static_cast<double>(child_offset)/2));
+
+        it_parent = m_Queue.begin() + parent_offset;
+        it_child = m_Queue.begin() + child_offset;
+
+        if ((*it_parent)->priority < (*it_child)->priority) {
                PriorityQueueNode* temp = (*it_parent);
                (*it_parent) = (*it_child);
-               (*it_child) = temp;               
-         } else {
+               (*it_child) = temp;
+        } else {
                break;
-         }
-         child_offset = parent_offset;
-     } 
+        }
+        child_offset = parent_offset;
+     }
 }
 
+
 std::string PriorityQueue::next(void) {
-    if(m_Queue.size() == 0) {
+    if (m_Queue.size() == 0) {
         throw new std::underflow_error("No element left in the PriorityQueue");
-    }     
+    }
 
     PriorityQueueNode* max_node = *m_Queue.begin();
     *m_Queue.begin() = *(m_Queue.end() - 1);
     std::string max = max_node->data;
-    m_Queue.erase(m_Queue.end() - 1); 
+    m_Queue.erase(m_Queue.end() - 1);
     bubbleDown();
     return max;
 }
 
 
 void PriorityQueue::bubbleDown(void) {
-     int parent_offset = 0;
-     int child_offset;
-    
-     std::vector<PriorityQueueNode*>::iterator it_parent;
-     std::vector<PriorityQueueNode*>::iterator it_child;
-     
-     while(parent_offset < m_Queue.size()) {
-         child_offset = findChildOffset(parent_offset);
-         if(child_offset < 0) break;
+    int parent_offset = 0;
+    int child_offset;
 
-         it_parent = m_Queue.begin() + parent_offset;
-         it_child = m_Queue.begin() + child_offset;
+    std::vector<PriorityQueueNode*>::iterator it_parent;
+    std::vector<PriorityQueueNode*>::iterator it_child;
 
-         if((*it_parent)->priority < (*it_child)->priority) {
+    while (parent_offset < m_Queue.size()) {
+        child_offset = findChildOffset(parent_offset);
+        if (child_offset < 0) break;
+
+        it_parent = m_Queue.begin() + parent_offset;
+        it_child = m_Queue.begin() + child_offset;
+
+        if ((*it_parent)->priority < (*it_child)->priority) {
                PriorityQueueNode* temp = (*it_parent);
                (*it_parent) = (*it_child);
-               (*it_child) = temp;   
-         }
-         else {
-              break;
-         }
-         parent_offset = child_offset;
+               (*it_child) = temp;
+        } else {
+            break;
+        }
+        parent_offset = child_offset;
      }
 }
 
 
 int PriorityQueue::findChildOffset(int parent_offset) {
-     int child_offset;
-     int l_child_offset = 2 * parent_offset;
-     int r_child_offset = 2 * parent_offset + 1;
+    int child_offset;
+    int l_child_offset = 2 * parent_offset;
+    int r_child_offset = 2 * parent_offset + 1;
 
-     std::vector<PriorityQueueNode*>::iterator it_parent = m_Queue.begin() + parent_offset;
-     std::vector<PriorityQueueNode*>::iterator it_child;
+    std::vector<PriorityQueueNode*>::iterator it_parent =
+        m_Queue.begin() + parent_offset;
+    std::vector<PriorityQueueNode*>::iterator it_child;
 
-     if(l_child_offset > m_Queue.size()) {
-         return -1;
-     } else {
-          child_offset = l_child_offset;   
-     }
-     
-     if(r_child_offset < m_Queue.size()) {
-          child_offset = findChildNodeWithLargerValue(child_offset, r_child_offset);  
-     }
-     return child_offset;
-} 
+    if (l_child_offset > m_Queue.size()) {
+        return -1;
+    } else {
+         child_offset = l_child_offset;
+    }
+
+    if (r_child_offset < m_Queue.size()) {
+       child_offset = findChildNodeWithLargerValue(
+            child_offset, r_child_offset);
+    }
+    return child_offset;
+}
 
 
-int PriorityQueue::findChildNodeWithLargerValue(int ch_offset_1, int ch_offset_2) {
+int PriorityQueue::findChildNodeWithLargerValue(int ch_offset_1,
+    int ch_offset_2) {
      unsigned int p1 = (*(m_Queue.begin() + ch_offset_1))->priority;
      unsigned int p2 = (*(m_Queue.begin() + ch_offset_2))->priority;
-     if(p1 > p2) {
+     if (p1 > p2) {
          return ch_offset_1;
-     } 
+     }
      return ch_offset_2;
 }
